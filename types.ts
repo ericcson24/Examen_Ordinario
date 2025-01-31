90054d3d0b4f7c156c5cf486a5a1385685c7330a
ericcson24 <150656730+ericcson24@users.noreply.github.com>
2025-01-31 16:00:17 +0100

    Create types.ts

---
+++ types.ts
@@ -0,0 +1,15 @@
+import { ObjectId, OptionalId } from "mongodb";
+
+
+
+export type RestauranteModel =OptionalId<{
+    nombre: string,
+    direccion: string,
+    ciudad:string,
+    numero_telefono: string
+}>
+
+export type Api_Phone={
+    is_valid:boolean
+}
+
